0b2a5c9da70c01f241cfc000317b9f4652a198fc
p2740654 <p2740654@GWHXX283WIN014W>
2024-05-02 12:49:21 +0100

    added testing for Customer class

---
+++ Testing2/tstCustomer.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
