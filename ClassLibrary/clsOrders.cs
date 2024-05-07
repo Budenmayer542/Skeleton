d1e76e0657c29a877e0efdc5aa4e70af9aad9585
p2730535 <p2730535@my365.dmu.ac.uk>
2024-05-07 10:52:33 +0100

    Removed problematic files from class library and added some test methods

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -1,6 +1,12 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrders
     {
+        public object Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int OrderNo { get; set; }
+        public int ItemCount { get; set; }
     }
 }
--- Testing4/tstOrders.cs
+++ Testing4/tstOrders.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.CodeDom;
 
 namespace Testing4
 {
@@ -11,9 +12,70 @@ namespace Testing4
         public void InstanceOK()
         {
             //create an instance of the class we want to create
-            clsOrders AnOrders = new clsOrders();
+            clsOrders AnOrder = new clsOrders();
             //test to see that it exists
-            Assert.IsNotNull(AnOrders);
+            Assert.IsNotNull(AnOrder);
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the calss we want to create
+            clsOrders AnOrder = new clsOrders();
+            //create some test date to assign to the property
+            Boolean TestData = true;
+            //assign the date to the property
+            AnOrder.Active = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnOrder.Active, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnOrder.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void OrderNoPorpertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.OrderNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder, TestData);
+        }
+
+        [TestMethod]
+        public void ItemCountPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //Assign the data to the property
+            AnOrder.ItemCount = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder, TestData);
+        }
+
+        [TestMethod]
+        public void SubTotalPropertyOK()
+        {  //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //create some test data to assign to the property
+            Decimal TestData = 5.15;
+            //assign the data to the property
         }
     }
 }
