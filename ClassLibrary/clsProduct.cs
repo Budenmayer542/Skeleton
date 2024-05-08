f13e9d33164717bd10d11fe680f1ba0b73e48f2a
P2731599 <P2731599@GWHXX283WIN011W>
2024-05-08 12:53:15 +0100

    COMIC/PRODUCT update, testing

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -1,9 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsProduct
     {
         public clsProduct()
         {
         }
+
+        public bool Active { get; set; }
+
+        public DateTime ComicInitialReleaseDate { get; set; }
+        public int ComicID { get; set; }
     }
 }
