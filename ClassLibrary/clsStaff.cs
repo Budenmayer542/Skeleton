024730fa720befbe02d56f59b52ee4971d10e90e
p2731247 <p2731247@my365.dmu.ac.uk>
2024-05-08 12:56:53 +0100

    practical 7 done

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,20 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public clsStaff()
+        {
+
+        }
+
+        public bool Active { get; set; }
+        public string StaffRole { get; set; }
+        public int StaffNumber { get; set; }
+        public string StaffName { get; set; }
+        public string StaffAddress { get; set; }
+        public DateTime DateAdded { get; set; }
+        public double StaffSalary { get; set; }
     }
 }
--- Testing1/tstStaff.cs
+++ Testing1/tstStaff.cs
@@ -13,7 +13,100 @@ namespace Testing1
             //create an instance of the class we want to create
             clsStaff AnStaff = new clsStaff();
             //test to see that it exists
-            Assert.IsNotNull(AnStaff);
+            Assert.IsNotNull(AnStaff);           
+           
+        }
+
+        [TestMethod]
+        public void StaffNumber() 
+        {
+            //create an instanmce of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnStaff.StaffNumber = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.StaffNumber, TestData);
+        }
+
+        [TestMethod]
+
+        public void StaffRole() 
+        {
+            //create an instanmce of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Manager";
+            //assign the data to the property
+            AnStaff.StaffRole = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.StaffRole, TestData);
+        }
+
+        [TestMethod]
+        public void StaffName()
+        { 
+            //create an instanmce of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Kelvin";
+            //assign the data to the property
+            AnStaff.StaffName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.StaffName, TestData);
+        }
+
+        [TestMethod]
+        public void StaffAddress()
+        {
+            //create an instanmce of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Leicester";
+            //assign the data to the property
+            AnStaff.StaffAddress = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.StaffAddress, TestData);
+        }
+
+        [TestMethod]
+        public void StaffDOB()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnStaff.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void StaffPriviliges() 
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnStaff.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.Active, TestData);
+        }
+
+        [TestMethod]
+        public void StaffSalary()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            double TestData = 20000;
+            //assign the data to the property
+            AnStaff.StaffSalary = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.Active, TestData);
         }
     }
 }
