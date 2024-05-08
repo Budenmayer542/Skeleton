6118fab4c131084cc1af442a4c5c04bd2ce81455
p2730535 <p2730535@my365.dmu.ac.uk>
2024-05-08 11:24:09 +0100

    Completed TestMethods

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -4,9 +4,12 @@ namespace ClassLibrary
 {
     public class clsOrders
     {
-        public object Active { get; set; }
         public DateTime DateAdded { get; set; }
         public int OrderNo { get; set; }
         public int ItemCount { get; set; }
+        public bool Dispatched { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+        public string DeliveryNote { get; set; }
     }
 }
