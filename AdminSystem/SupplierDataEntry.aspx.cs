f4fb1c7ca02b9804900297f225e36da7c2dc8def
P2731494 <P2731494@my365.dmu.ac.uk>
2024-05-09 14:57:07 +0100

    finished off practical 9a and added find tests to clssupplier and tstsupplier

--- AdminSystem/SupplierDataEntry.aspx.cs
+++ AdminSystem/SupplierDataEntry.aspx.cs
@@ -16,16 +16,13 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void btnOk_Click(object sender, EventArgs e)
     {
         clsSupplier AnSupplier = new clsSupplier(); //new instance
-        AnSupplier.SupplierName = txtSupplierName.Text; //capture name
+        AnSupplier.SupplierId = Convert.ToInt32(txtSupplierID.Text); //capture ID
+        AnSupplier.SupplierName = txtSupplierName.Text; 
         AnSupplier.ContactPerson = txtContactPerson.Text;
         AnSupplier.SupplierEmail = txtSupplierEmail.Text;
         AnSupplier.SupplierTelephone = txtSupplierTelephone.Text;
         AnSupplier.InitialContractDate = Convert.ToDateTime(DateTime.Now);
         AnSupplier.SupplierActive = chkActive.Checked;
-        
-        
-
-
         Session["AnSupplier"] = AnSupplier; // store name in session object
         Response.Redirect("SupplierViewer.aspx"); //navigate to view page
     }
--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,14 +1,124 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
-        public int SupplierId { get; set; }
-        public object SupplierActive { get; set; }
-        public object InitialContractDate { get; set; }
-        public string SupplierName { get; set; }
-        public string ContactPerson { get; set; }
-        public string SupplierEmail { get; set; }
-        public string SupplierTelephone { get; set; }
+        private Int32 mSupplierId; //private data member for the supplier id property
+        public Int32 SupplierId //public property 
+        {
+            get
+            {
+                return mSupplierId; //sends data out of the property 
+            }
+            set
+            {
+                mSupplierId = value; //allows data into the property 
+            }
+        }
+
+        private String mSupplierName; //private data member for the supplier name property
+        public string SupplierName  
+        {
+            get
+            {
+                return mSupplierName;  
+            }
+            set
+            {
+                mSupplierName = value;  
+            }
+        }
+
+        private String mContactPerson; //private data member for the contact person property
+        public string ContactPerson 
+        {
+            get
+            {
+                return mContactPerson; 
+            }
+            set
+            {
+                mContactPerson = value; 
+            }
+        }
+
+        private String mSupplierEmail; //private data member for the supplier email property
+        public string SupplierEmail
+        {
+            get 
+            {
+                return mSupplierEmail;
+            }
+            set 
+            { 
+                mSupplierEmail = value; 
+            }
+        }
+
+        private String mSupplierTelephone; //private data member for the supplier telephone property
+        public string SupplierTelephone
+        {
+            get
+            {
+                return mSupplierTelephone ;
+            }
+            set
+            {
+                mSupplierEmail = value;
+            }
+        }
+
+        private DateTime mInitialContractDate; //private data member for the initial contract date property
+        public DateTime InitialContractDate 
+        {
+            get
+            {
+                return mInitialContractDate;
+            }
+            set
+            {
+                mInitialContractDate = value;
+            }
+        }
+
+        private Boolean mSupplierActive; //private data member for the supplier active property
+        public bool SupplierActive
+        {
+            get
+            {
+                return mSupplierActive;
+            }
+            set
+            {
+                mSupplierActive = value;
+            }
+        }
         
+        
+        
+
+
+
+
+
+
+
+
+        public bool Find(int SupplierId)
+        {
+            mSupplierId = 21; //set the private data member to the test data value
+            mSupplierName = "Manga Comics ltd";
+            mContactPerson = "jeff";
+            mSupplierEmail = "mangacomics@outlook.com";
+            mSupplierTelephone = "07924 385853";
+            mInitialContractDate = Convert.ToDateTime("23/12/2022");
+            mSupplierActive = true;
+            return true; //always return true
+        }
+
+      
+
+
     }
 }
