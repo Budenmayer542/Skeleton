919c758f48a5020241c4cdbf18008e2719e8d6c6
P2731599 <P2731599@GWHXX283WIN011W>
2024-05-09 11:51:15 +0100

    Practical 7 completed :fire:

--- Testing5/tstProduct.cs
+++ Testing5/tstProduct.cs
@@ -28,6 +28,24 @@ namespace Testing5
         }
 
         [TestMethod]
+        public void ComicIdPropertyOK()
+        {
+            clsProduct AProduct = new clsProduct();
+            Int32 TestData = 1;
+            AProduct.ComicID = TestData;
+            Assert.AreEqual(AProduct.ComicID, TestData);
+
+        }
+        [TestMethod]
+        public void ComicNameProperty()
+        {
+            clsProduct AProduct = new clsProduct();
+            string TestData = "Mirai Nikki Volume 1";
+            AProduct.ComicName = TestData;
+            Assert.AreEqual(AProduct.ComicName, TestData);
+        }
+
+        [TestMethod]
         public void ComicInitialReleaseDatePropertyOK() 
         {
             clsProduct AProduct = new clsProduct();
@@ -37,13 +55,39 @@ namespace Testing5
         }
 
         [TestMethod]
-        public void ComicIdPropertyOK ()
+        public void ComicArtistProperty()
         {
             clsProduct AProduct = new clsProduct();
-            Int32 TestData = 1;
-            AProduct.ComicID = TestData;
-            Assert.AreEqual(AProduct.ComicID, TestData);
+            string TestData = "Sakae Esuno";
+            AProduct.ComicArtist = TestData;
+            Assert.AreEqual(AProduct.ComicArtist, TestData);
+        }
 
+        [TestMethod]
+        public void ComicPriceProperty() 
+        {
+            clsProduct AProduct = new clsProduct();
+            double TestData = 7.99;
+            AProduct.ComicPrice = TestData;
+            Assert.AreEqual(AProduct.ComicPrice, TestData);
+        }
+
+        [TestMethod]
+        public void ComicGenreProperty() 
+        {
+            clsProduct AProduct = new clsProduct();
+            string TestData = "Action";
+            AProduct.ComicGenre = TestData;
+            Assert.AreEqual(AProduct.ComicGenre, TestData);
+        }
+
+        [TestMethod]
+        public void ComicIsComicInStock () 
+        { 
+            clsProduct AProduct = new clsProduct(); 
+            Boolean TestData = true;
+            AProduct.IsComicInStock =  TestData;
+            Assert.AreEqual(AProduct.IsComicInStock, TestData);
         }
     }
 }
