54a91c9de5f310165cc7ffd41f0559b53124be44
p2740654 <p2740654@GWHXX283WIN014W>
2024-05-12 13:19:24 +0100

    test2

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public string EmailAddress;
+        public int CustomerId;
+        public int PhoneNumber;
+
+        public bool Active { get; set; }
+        public DateTime SignUp { get; set; }
+        public string FullName { get; set; }
     }
 }
--- Testing2/Customer.cs
+++ Testing2/Customer.cs
@@ -16,5 +16,70 @@ namespace Testing2
             //test to see that it exits
             Assert.IsNotNull(AnCustomer);
         }
+        public void Activepropertyok()
+        {
+            //create an instance of the class we want to create 
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnCustomer.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.Active, TestData);
+        }
+        public void CustomerIdpropertyok()
+        { //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the  property
+            AnCustomer.CustomerId = TestData;
+            //test to see that the two vlaues are the same
+            Assert.AreEqual(AnCustomer.CustomerId, TestData);
+        }
+
+        public void SignUppropertyok()
+        {
+            //creat an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now;
+            //assign the data to the property
+            AnCustomer.SignUp = TestData;
+            //test to see that the two values are the same
+            Assert.IsNotNull(AnCustomer.SignUp, TestData);
+        }
+        public void FullNamepropertyok()
+        { //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            String TestData = "Smily";
+            //assign the data to the property
+            AnCustomer.FullName = TestData;
+            //test to see that the two values are the same
+            Assert.IsNotNull(AnCustomer.FullName, TestData);
+        }
+        public void EmailAddresspropertyok()
+        { //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+            String TestData = "smily74@gmail.com";
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+            //aasign the data to the property 
+            AnCustomer.EmailAddress = TestData;
+            //test to see that the two values are the same
+            Assert.IsNotNull(AnCustomer.EmailAddress, TestData);
+        }
+        public void PhoneNumberproperty()
+        { //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            Int32 TestData = 0743684998;
+            //assign the dat to the property
+            AnCustomer.PhoneNumber = TestData;
+            //test to see that the two values are the same
+            Assert.IsNotNull(AnCustomer.PhoneNumber, TestData);
+        }
     }
 }
