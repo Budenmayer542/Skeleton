ff494607fab789dc3db9182629fcf1110484982c
p2740654 <p2740654@GWHXX283WIN014W>
2024-05-13 15:15:24 +0100

    pratical 6 complete

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,15 +1,6 @@
-﻿using System;
-
-namespace ClassLibrary
+﻿namespace ClassLibrary
 {
     public class clsCustomer
     {
-        public string EmailAddress;
-        public int CustomerId;
-        public int PhoneNumber;
-
-        public bool Active { get; set; }
-        public DateTime SignUp { get; set; }
-        public string FullName { get; set; }
     }
 }
---
+++ Testing2/tstCustomer1.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomer1
+    {
+        [TestMethod]
+        public void Instanceok()
+        {//create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //test to see that it's exists
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
