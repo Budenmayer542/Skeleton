8d42c1b75d593ef9fd89369ef71e54fb4c6bd9e4
P2731599 <P2731599@GWHXX283WIN011W>
2024-05-14 14:57:14 +0100

    Practical 9a start

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -6,16 +6,34 @@ namespace ClassLibrary
     {
         public clsProduct()
         {
+            
         }
 
         public bool Active { get; set; }
 
         public DateTime ComicInitialReleaseDate { get; set; }
-        public int ComicID { get; set; }
         public string ComicName { get; set; }
         public string ComicArtist { get; set; }
         public double ComicPrice { get; set; }
         public string ComicGenre { get; set; }
         public bool IsComicInStock { get; set; }
+
+        private Int32 mComicID;
+
+        public Int32 ComicID
+        {
+            
+                get
+                        { return mComicID; }
+            
+            set { mComicID = value; }
+        }
+        public bool Find(int ComicID)
+        {
+            //Always returns true
+            mComicID = 1;
+            return true;
+        }
+
     }
 }
