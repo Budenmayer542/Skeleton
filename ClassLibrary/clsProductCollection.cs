b2bbc4348ebd033b6abe15e1cf7540b2c8422790
P2731599 <P2731599@GWHXX283WIN011W>
2024-05-22 22:51:07 +0100

    14b almost done (Library pc wants to update in 10 minutes)

--- ClassLibrary/clsProductCollection.cs
+++ ClassLibrary/clsProductCollection.cs
@@ -48,29 +48,11 @@ namespace ClassLibrary
 
         public clsProductCollection()
         {
-            //Variable for the index
-            Int32 Index = 0;
-            //Variable to store the record count
-            Int32 RecordCount = 0;
-            //Object for the data connection
             clsDataConnection DB = new clsDataConnection();
             //Execute the stored procedure
             DB.Execute("tblComic_SelectAll");
-            //Get the count of records
-            RecordCount = DB.Count;
-            while (Index < RecordCount)
-            {
-                clsProduct AProduct = new clsProduct();
-                AProduct.ComicID = Convert.ToInt32(DB.DataTable.Rows[Index]["ComicID"]);
-                AProduct.ComicName = Convert.ToString(DB.DataTable.Rows[Index]["ComicName"]);
-                AProduct.ComicInitialReleaseDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["ComicInitialReleaseDate"]);
-                AProduct.ComicArtist = Convert.ToString(DB.DataTable.Rows[Index]["ComicArtist"]);
-                AProduct.ComicPrice = Convert.ToDouble(DB.DataTable.Rows[Index]["ComicPrice"]);
-                AProduct.ComicGenre = Convert.ToString(DB.DataTable.Rows[Index]["ComicGenre"]);
-                AProduct.IsComicInStock = Convert.ToBoolean(DB.DataTable.Rows[Index]["IsComicInStock"]);
-                mProductList.Add(AProduct);
-                Index++;
-            }
+            //Populate thhe array list with the data table
+            PopulateArray(DB);
 
         }
 
@@ -109,6 +91,49 @@ namespace ClassLibrary
             clsDataConnection DB = new clsDataConnection();
             DB.AddParameter("@ComicID", mThisProduct.ComicID);
             DB.Execute("sproc_tblComic_Delete");
+        }
+
+        public void ReportByComicName(string ComicName)
+        {
+            //filters the records based on a full or parial comic names
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //Send the ComicName parameter to the database
+            DB.AddParameter("@ComicName", ComicName);
+            //Execute the stored procedure
+            DB.Execute("sproc_tblComic_FilterByComicName");
+            //Populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //Variable for the index
+            Int32 Index = 0;
+            //Variable to store the record count
+            Int32 RecordCount;
+            //Get the count of records
+            RecordCount = DB.Count;
+            //Clear the private array list
+            mProductList = new List<clsProduct>();
+            //While there are records to process
+            while (Index < RecordCount)
+            {
+                //Create a blank address object
+                clsProduct AProduct = new clsProduct();
+                //Read in the fields from the current rcord
+                AProduct.ComicID = Convert.ToInt32(DB.DataTable.Rows[Index]["ComicID"]);
+                AProduct.ComicName = Convert.ToString(DB.DataTable.Rows[Index]["ComicName"]);
+                AProduct.ComicInitialReleaseDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["ComicInitialReleaseDate"]);
+                AProduct.ComicArtist = Convert.ToString(DB.DataTable.Rows[Index]["ComicArtist"]);
+                AProduct.ComicPrice = Convert.ToDouble(DB.DataTable.Rows[Index]["ComicPrice"]);
+                AProduct.ComicGenre = Convert.ToString(DB.DataTable.Rows[Index]["ComicGenre"]);
+                AProduct.IsComicInStock = Convert.ToBoolean(DB.DataTable.Rows[Index]["IsComicInStock"]);
+                //Add the record to the private data member
+                mProductList.Add(AProduct);
+                //Point at the next record
+                Index++;
+            }
         }
     }
 }
