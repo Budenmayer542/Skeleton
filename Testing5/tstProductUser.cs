d145645aa2f8efc8826fe36e7893f82051986eaf
P2731599 <P2731599@GWHXX283WIN011W>
2024-05-28 10:53:36 +0100

    Practical 15 DONE !!! :fire:

---
+++ Testing5/tstProductUser.cs
@@ -0,0 +1,70 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstProductUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsProductUser AnUser = new clsProductUser();
+            //Test to see that it exists
+            Assert.IsNotNull(AnUser);
+        }
+
+        [TestMethod]
+        public void UserIDPropertyOK()
+        {
+            //Create an instance of the class we want to create
+            clsProductUser AnUser = new clsProductUser();
+            //Create some test data to assign to the property
+            Int32 TestData = 1;
+            //Assign the data to the property
+            AnUser.UserID = TestData;
+            //Test to see that the two valuesare the same
+            Assert.AreEqual(AnUser.UserID, TestData);
+        }
+
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProductUser AnUser = new clsProductUser();
+            //create some test data to assign to the property
+            string TestData = "Dawn";
+            //assign the data to the property
+            AnUser.UserName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.UserName, TestData);
+        }
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProductUser AnUser = new clsProductUser();
+            //create some test data to assign to the property
+            string TestData = "password123";
+            //assign the data to the property
+            AnUser.Password = TestData;
+            //test to see that the two values are the sam
+        }
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProductUser AnUser = new clsProductUser();
+            //create some test data to assign to the property
+            string TestData = "Address Book";
+            //assign the data to the property
+            AnUser.Department = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+    }
+}
