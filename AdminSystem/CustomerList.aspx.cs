e368fe3fd03aae4527a7bd0d83f1f7b87ea8eac9
p2740654 <p2740654@GWHXX283WIN014W>
2024-05-29 20:51:59 +0100

    completed pratical 12

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,24 @@ public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //updete the list box
+            DisplayCustomers();
+        }
+    }
+    void DisplayCustomers()
+    {
+        //create an instance of the customer collection
+        clsCustomerCollection Customers = new clsCustomerCollection();
+        //set the data source to list of the customer in the collection
+        lstCustomerList.DataSource = Customers.CustomerList;
+        //set the name of the primary key
+        lstCustomerList.DataValueField = "CustomerId";
+        //set the data field to display
+        lstCustomerList.DataTextField = "FullName";
+        //bind the data to the list
+        lstCustomerList.DataBind();
     }
 }
